f120d2c290ce885a786e0371ce5060f5cd612508
ZezoKing <44140265+ZezoKing@users.noreply.github.com>
2019-11-19 20:42:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', async() => {
-var server = "619139460092788746"; 
-var channel = "646390015563137025";
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('ياويل امها طاط')
-    },305);
-}) 
+async def on_message(message):
+    if message.content.upper().startswith('-SAY'):
+        if message.author.id == "608511451736571914":
+        userID = message.author.id
+            args = message.content.split(" ")
+            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+        else:
+            await client.send_message(message.channel, "You do not have permission")
 
 
 client.login(process.env.BOT_TOKEN);
